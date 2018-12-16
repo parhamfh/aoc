a636068e0544ac7be78889fcbd12cde545de939a
Pärham Fazelzadeh <parhamfh@kth.se>
2018-12-16 04:10:04 +0100

    Solved 2B

--- 2018/2/B.hs
+++ 2018/2/B.hs
@@ -7,18 +7,19 @@ main = do
     print $ common_for_close ids
 
 common_for_close :: [String] -> String
-common_for_close (id:ids) = find_close id ids
+common_for_close (id:ids) = find_close id [] ids
 
-find_close :: String -> [String] -> String 
-find_close id [i] = ""
-find_close id (i:ids)
+find_close :: String -> [String] -> [String] -> String 
+find_close id [] [i] = ""
+find_close id (i_other:other) [i] = find_close i_other [] other
+find_close id other_ids (i:ids)
     | close False id i      = intersect id i
-    | otherwise             = find_close id ids
+    | otherwise             = find_close id (i:other_ids) ids
 
 close :: Bool -> String -> String -> Bool
+close one_diff [] []                    = True
 close one_diff (a:as) (b:bs)
     | a == b                            = close one_diff as bs
     | a /= b && (one_diff == True)      = False
     | a /= b && (one_diff == False)     = close True as bs
-    | otherwise                         = True
 
