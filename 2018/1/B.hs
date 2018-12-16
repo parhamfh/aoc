fe216849db24481b9e6e5f2da506c000fe0fbbf3
Pärham Fazelzadeh <parhamfh@kth.se>
2018-12-16 01:38:08 +0100

    Completed 1st day

--- 2018/1/B.hs
+++ 2018/1/B.hs
@@ -9,39 +9,29 @@ main = do
     
     -- print all frequencies
     -- mapM print freqs
-    print $ checkFreq 0 IS.empty freqs
+    let freq_list = map (\x -> mapToInt x) freqs
+    print $ show $ calibrate freq_list
 
-checkFreq :: Int -> IS.IntSet -> [String] -> Int
-checkFreq tot_freq freq_set [f]
-    | t $ IS.member new_tot freq_set    = new_tot
-    | otherwise                     = -1
-    where
-        new_tot = calcFreq tot_freq f
-        t = trace (
-                "calling checkFreq SINGLE with tot_freq = " ++ 
-                    (show tot_freq :: String) ++ 
-                " and freq_set is = " ++ 
-                    (intercalate "," (map (\x -> (show (x) :: String)) (IS.elems freq_set)) ) 
-            ) 
-checkFreq tot_freq freq_set (f:f_remain)
-    | t $ IS.member new_tot freq_set    = new_tot
-    | t $ otherwise                     = checkFreq new_tot (IS.insert new_tot freq_set) f_remain
-    where
-        new_tot = calcFreq tot_freq f
-        t = trace (
-                "calling checkFreq LIST with tot_freq = " ++ 
-                    (show tot_freq :: String) ++ 
-                " and freq_set is = " ++ 
-                    (intercalate "," (map (\x -> (show (x) :: String)) (IS.elems freq_set)) ) 
-            ) 
+calibrate :: [Int] -> Int
+calibrate freqs = checkFreq 0 IS.empty [] freqs
 
-calcFreq :: Int -> String -> Int
-calcFreq total (sign:number)
-    | sign == '+'   = t_add $ total + val
-    | sign == '-'   = t_sub $ total - val
+checkFreq :: Int -> IS.IntSet -> [Int] ->  [Int] -> Int
+checkFreq tot_freq freq_set next_set  [f]
+    | IS.member new_tot freq_set    = new_tot
+    | otherwise                     = checkFreq new_tot (IS.insert new_tot freq_set) [] (reverse (f:next_set)) 
+    where
+        new_tot = tot_freq + f
+
+checkFreq tot_freq freq_set next_set (f:f_remain)
+    | IS.member new_tot freq_set    = new_tot
+    | otherwise                     = checkFreq new_tot (IS.insert new_tot freq_set) (f:next_set) f_remain
+    where
+        new_tot = tot_freq + f
+
+mapToInt :: String -> Int
+mapToInt (sign:number)
+    | sign == '+'   = val
+    | sign == '-'   = -1 * val
     where 
         val = (read number :: Int)
-        t_add = trace ("addng val " ++ number ++ " to " ++ (show total) ++ " => " ++ (show $ total + val) ) 
-        t_sub = trace ("subtr val " ++ number ++ " fr " ++ (show total) ++ " => " ++ (show $ total - val) )
-
 
